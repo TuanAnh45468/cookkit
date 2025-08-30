1e2c0e6525ed121b1dbc904f42f5a53ec85c5f6a
TuanAnh <tuananh45468@gmail.com>
2025-08-30 23:29:58 +0700

    feat(stories): add Storybook setup with Button component and configuration

---
+++ src/components/button/__docs__/Example.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react";
+import {ButtonProps} from "../button";
+import {Button} from "../index";
+
+const Example: FC<ButtonProps> = ({
+  disabled = false,
+  onClick = () => {},
+  primary = true,
+  size = "small",
+  text = "Button",
+}) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+      }}
+    >
+      <Button
+        size={size}
+        text={text}
+        disabled={disabled}
+        onClick={onClick}
+        primary={primary}
+      />
+    </div>
+  );
+};
+
+export default Example;
